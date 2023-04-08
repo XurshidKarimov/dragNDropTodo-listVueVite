0570c2b65fedb9ffa003006325927691cfc16b5b
unknown <kaxudi34801@gmail.com>
2023-04-08 23:22:40 +0500

    add component. add some styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  
+  <main>
+    <div class="container">
+      <div class="dragNdrop-content">
+        <drag-n-drop />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
+import DragNDropVue from "../../VueViteSandbox/src/components/DragNDrop.vue";
+
 
 </script>
 
 
 <style scoped>
-
+main{
+    width: 100%;
+  }
+  .container{
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 15px;
+  }
 </style>
